8586e92ae4071349f6742f58b2c7add1dd007725
Wangtomorrow <914606466@qq.com>
2018-03-21 22:41:25 +0800

    排列组合

---
+++ 全排列.cpp
@@ -0,0 +1,43 @@
+﻿/*
+请编写程序输出前n个正整数的全排列（n<10），并通过9个测试用例（即n从1到9）观察n逐步增大时程序的运行时间。
+
+输入格式:
+输入给出正整数n（<10）。
+
+输出格式:
+输出1到n的全排列。每种排列占一行，数字间无空格。排列的输出顺序为字典序，
+输入样例：
+3
+输出样例：
+123
+132
+213
+231
+312
+321
+*/
+#include<iostream>  
+#include<algorithm>  
+using namespace std;  
+int main()  
+{  
+    int n;  
+    cin>>n;  
+    int a[100];  
+    for(int i = 1; i <= n;i++)  
+    {  
+        a[i-1] = i;  
+    }  
+    do  
+    {  
+        for(int i = 0; i < n;i++)  
+        {  
+            cout<<a[i];  
+        }  
+        cout<<endl;  
+    }  
+    while (next_permutation(a,a+n));  
+    return 0;  
+} 
+
+//全排列 
---
+++ 排列组合.cpp
@@ -0,0 +1,35 @@
+﻿/*
+从n个不同元素中取出m个元素（m≤n）的组合数。
+输入格式:
+输入在一行中给出两个正整数m和n（m≤n），以空格分隔。
+
+输出格式:
+按照格式“result = 组合数计算结果”输出。题目保证结果在double类型范围内。
+
+输入样例:
+2 7
+输出样例:
+result = 21
+*/
+#include <stdio.h>
+#include<string.h>
+#include<iostream>
+#include <sstream>
+using namespace std;
+double fun(int n)
+{   
+    if(n<=1)  
+        return 1;  
+    return n*fun(n-1);  
+}   
+int main()
+{  
+    int m,n; 
+	double result; 
+    cin >> m >> n; 
+    result=fun(n)/(fun(m)*fun(n-m));  
+    cout << "result = "<< result << endl;  
+    return 0;  
+}  
+  
+//排列组合公式 
